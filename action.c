fe63b109c801e29f973657bc7fc7f3ba3af1f3f6
noeLienard <noelienard2006@gmail.com>
2025-08-08 16:01:12 +0200

    hello

--- action.c
+++ action.c
@@ -6,15 +6,15 @@
 /*   By: nlienard <nlienard@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/04 12:24:52 by nlienard          #+#    #+#             */
-/*   Updated: 2025/08/08 13:11:37 by nlienard         ###   ########.fr       */
+/*   Updated: 2025/08/08 15:50:22 by nlienard         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philosophers.h"
 
-int	take_fork_right(t_args *lc_args, t_philo *philo)
+int	take_fork_right(t_args *lc_args, t_philo *philo, int i)
 {
-	pthread_mutex_lock(&lc_args->mtx_fork[lc_args->i]);
+	pthread_mutex_lock(&lc_args->mtx_fork[i]);
 	pthread_mutex_lock(&lc_args->mtx_print);
 	printf_action((get_timestamp() - lc_args->start_time), philo->idx,
 		"has taken a fork");
@@ -22,9 +22,9 @@ int	take_fork_right(t_args *lc_args, t_philo *philo)
 	return (0);
 }
 
-int	take_fork_left(t_args *lc_args, t_philo *philo)
+int	take_fork_left(t_args *lc_args, t_philo *philo, int i)
 {
-	pthread_mutex_lock(&lc_args->mtx_fork[(lc_args->i + 1) % lc_args->nbr_p]);
+	pthread_mutex_lock(&lc_args->mtx_fork[(i + 1) % lc_args->nbr_p]);
 	pthread_mutex_lock(&lc_args->mtx_print);
 	printf_action((get_timestamp() - lc_args->start_time), philo->idx,
 		"has taken a fork");
@@ -32,17 +32,17 @@ int	take_fork_left(t_args *lc_args, t_philo *philo)
 	return (0);
 }
 
-int	is_eating(t_args *lc_args, t_philo *philo)
+int	is_eating(t_args *lc_args, t_philo *philo, int i)
 {
 	pthread_mutex_lock(&lc_args->mtx_print);
 	usleep(lc_args->time_to_eat);
 	printf_action((get_timestamp() - lc_args->start_time), philo->idx,
 		"is eating");
+	pthread_mutex_unlock(&lc_args->mtx_print);
 	philo->nbr_meal++;
 	philo->last_meal = get_timestamp();
-	pthread_mutex_unlock(&lc_args->mtx_print);
-	pthread_mutex_unlock(&lc_args->mtx_fork[lc_args->i]);
-	pthread_mutex_unlock(&lc_args->mtx_fork[(lc_args->i + 1) % lc_args->nbr_p]);
+	pthread_mutex_unlock(&lc_args->mtx_fork[i]);
+	pthread_mutex_unlock(&lc_args->mtx_fork[(i + 1) % lc_args->nbr_p]);
 	return (1);
 }
 
@@ -98,18 +98,19 @@ void	*ft_monitoring(void *args)
 
 void	*action_routine(void *args)
 {
+	int i;
 	t_args	*lc_args;
-
 	lc_args = (t_args *)args;
-	if (lc_args->i % 2 != 0)
+	i = lc_args->i;
+	if (i % 2 != 0)
 		usleep(1000);
 	while (1)
 	{
-		take_fork_right(lc_args, &lc_args->tab_philo[lc_args->i]);
-		take_fork_left(lc_args, &lc_args->tab_philo[lc_args->i]);
-		is_eating(lc_args, &lc_args->tab_philo[lc_args->i]);
-		is_sleeping(lc_args, &lc_args->tab_philo[lc_args->i]);
-		is_thinking(lc_args, &lc_args->tab_philo[lc_args->i]);
+		take_fork_right(lc_args, &lc_args->tab_philo[i], i);
+		take_fork_left(lc_args, &lc_args->tab_philo[i], i);	
+		is_eating(lc_args, &lc_args->tab_philo[i], i);
+		is_sleeping(lc_args, &lc_args->tab_philo[i]);
+		is_thinking(lc_args, &lc_args->tab_philo[i]);
 	}
 	return (NULL);
 }
